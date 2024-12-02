4f92026c4b204f0438d5695a4e6c56f2826868d1
Ivan Epou <ivanepou92@gmail.com>
2024-12-02 13:57:50 +0300

    Add forms

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,9 +3,5 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <main>
-        {children}
-    </main>
-  );
+  return <main className="font-inter">{children}</main>;
 }
---
+++ components/CustomInput.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { FormControl, FormField, FormLabel, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
+import { Control, FieldPath } from "react-hook-form";
+import { authFormSchema } from "@/lib/utils";
+import { z } from "zod";
+
+interface CustomInput {
+  control: Control<z.infer<typeof authFormSchema>>;
+  name: FieldPath<z.infer<typeof authFormSchema>>;
+  label: string;
+  placeholder: string;
+  type: string;
+}
+
+const CustomInput = ({
+  control,
+  name,
+  label,
+  placeholder,
+  type,
+}: CustomInput) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <div className="form-item">
+          <FormLabel className="form-label">{label}</FormLabel>
+          <div className="flex w-full flex-col">
+            <FormControl>
+              <Input
+                placeholder={placeholder}
+                className="input-class"
+                type={type}
+                {...field}
+              />
+            </FormControl>
+            <FormMessage className="form-message mt-2"></FormMessage>
+          </div>
+        </div>
+      )}
+    />
+  );
+};
+
+export default CustomInput;
